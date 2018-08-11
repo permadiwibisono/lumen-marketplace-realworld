072b6e1201b7323ed11f41117acc5afe1fb82f17
Permadi Wibisono <mbapewe@gmail.com>
2018-08-11 23:58:50 +0700

    Setting morphMap for alias type and Observer but still not works

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,34 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    User::observe(UserObserver::class);
+    Relation::morphMap([
+      'user' => 'App\Models\User'
+    ]);
+  }
+
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+      //
+  }
 }
